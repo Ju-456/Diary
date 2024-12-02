fff1a355ca29d391dddb7ebaf86f2dc2ea4b075e
Juliena <juliena456@gmail.com>
2024-12-02 10:19:23 +0100

    just basic struct and comment about goals

--- JavaVersionOfDiary/source/main/java/com/exemple/application/Main.java
+++ JavaVersionOfDiary/source/main/java/com/exemple/application/Main.java
@@ -1,5 +1,7 @@
 package com.exemple.application;  // Ton package
 
+import java.awt.Color;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -9,5 +11,15 @@ import javafx.stage.Stage;
 public class Main extends Application{
 
     @Override
-    public void 
+    public void start(Stage primaryStage) {
+        
+        scene.setFill(Color.DARK_GRAY); 
+        //root.setStyle("-fx-background-color: #f0f0f0;");
+        //integration of Scene of scenes 1 to 5 bis
+        //integration of buttons
+        //integration of "precedent" to go back
+
+        //thinking about style and display
+        
+    }
 }
